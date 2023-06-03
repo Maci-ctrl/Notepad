927a05e50d50c4445a2e5d6318ec9236d12cc682
AgnieszkaSow <125731356+AgnieszkaSow@users.noreply.github.com>
2023-06-03 10:22:37 +0200

    Creating notepad context wip

---
+++ Notepad/Data/NotepadContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notepad.Models;
+
+namespace Notepad.Data
+{
+    public class NotepadContext : DbContext
+    {
+        public NotepadContext(DbContextOptions<NotepadContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Note>(entity =>);
+            {
+                
+            }
+        }
+    }
+}
