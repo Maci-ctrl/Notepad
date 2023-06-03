5c28a2c0364c1b9bc13d0397e59619deaffccd7f
Adam Chudzyński <achudzynski@gmail.com>
2023-06-03 10:02:48 +0200

    updated controller

--- Notepad/Controllers/NoteController.cs
+++ Notepad/Controllers/NoteController.cs
@@ -23,5 +23,42 @@ namespace Notepad.Controllers
 		{
 			return View();
 		}
+
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			var model =  _db.Get(id);
+			return View(model);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Note note)
+		{
+			return View();
+		}
+
+
+		[HttpGet]
+		public async IActionResult Delete(int id)
+		{
+			var model = _db.Get(id);
+
+			if (model == null)
+			{
+				return View("NotFound");
+			}
+			return View(model);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Delete(int id)
+		{
+			_db.Delete(id);
+
+			return RedirectToAction("Index");
+		}
 	}
 }
+
