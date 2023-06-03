647ab61bbe628208b47f087d49dbfc3123f8b30d
AgnieszkaSow <125731356+AgnieszkaSow@users.noreply.github.com>
2023-06-03 09:43:08 +0200

    Note constructor date created added

--- Notepad/Models/Note.cs
+++ Notepad/Models/Note.cs
@@ -1,6 +1,17 @@
-﻿namespace Notepad.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notepad.Models
 {
     public class Note
     {
+        [Key]
+       // [DatebaseGeneratedAtrribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+
     }
 }
