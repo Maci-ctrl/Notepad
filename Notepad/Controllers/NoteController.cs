f58d009665b1a04cc3c71ea7e9ea7b8b234535e8
Adam Chudzyński <achudzynski@gmail.com>
2023-06-03 09:52:36 +0200

    added controller

---
+++ Notepad/Controllers/NoteController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Notepad.Controllers
+{
+	public class NoteController : Controller
+	{
+		private readonly INotepadService _db;
+
+		public RestaurantsController(INotepadService db)
+		{
+			_db = db;
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Note note)
+		{
+			return View();
+		}
+	}
+}
