777e5bf804e6e5e91f4861089c874e2b3f2c8f30
Maci-ctrl <125731455+Maci-ctrl@users.noreply.github.com>
2023-06-03 10:21:05 +0200

    Create INoteService

--- Notepad/Services/INoteService.cs
+++ Notepad/Services/INoteService.cs
@@ -2,5 +2,24 @@
 {
     public interface INoteService
     {
+        int Id { get; set; }
+
+        string Title { get; set; }
+
+        string Content { get; set; }
+
+        DateTime DateCreate { get; set; }
+
+        DateTime DateUpdaet { get; set; }
+
+        void Get();
+
+        void GetAll();
+
+        void Add();
+
+        void Delete();
+
+        void Update();
     }
 }
