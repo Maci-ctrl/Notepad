4ca4b325c5ac69b4267dace4955969331687535b
Maci-ctrl <125731455+Maci-ctrl@users.noreply.github.com>
2023-06-04 15:45:30 +0200

    JS Function added

--- Notepad/wwwroot/js/site.js
+++ Notepad/wwwroot/js/site.js
@@ -4,20 +4,26 @@
 // Write your JavaScript code.
 
 
-function functionCheck() {
-    $('form > input').keyup(function () {
+(function () {
+    $('.validate').each(function () {
 
-        var empty = false;
-        $('form > input').each(function () {
-            if ($(this).val() == '') {
-                empty = true;
+        var empty = true;
+        $(this).keyup(function () {
+            
+            if ($(this).val() != '') {
+                empty = false;
             }
+            validateInput(empty)
         });
-
-        if (empty) {
-            $('#SaveButtonCreate').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
-        } else {
-            $('#SaveButtonCreate').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
-        }
+        validateInput(empty)
+        
     });
+})()
+
+function validateInput(value) {
+    if (value) {
+        $('#SaveButtonCreate').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+    } else {
+        $('#SaveButtonCreate').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+    }
 }
