5139d4085cc2cb56502c226a7cf11f983f6137e4
Maci-ctrl <125731455+Maci-ctrl@users.noreply.github.com>
2023-06-21 20:08:34 +0200

    class Form add props and constructor

--- Notepad/Models/Form.cs
+++ Notepad/Models/Form.cs
@@ -2,5 +2,23 @@
 {
     public class Form
     {
+        private int _id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string  Email { get; set; }
+
+        public DateOnly DateBirth { get; set; }
+
+        public Form (string firstName, string lastName, string email, DateOnly dateBirth)
+        {
+            _id++;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            DateBirth = dateBirth;
+        }
     }
 }
