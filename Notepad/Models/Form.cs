ad9ec12fa55129cbf7862327f74c78b81f603558
Maci-ctrl <125731455+Maci-ctrl@users.noreply.github.com>
2023-06-24 20:49:03 +0200

    add IFormService interface and implement it in FormService class modified file Index in Views/Form

--- Notepad/Models/Form.cs
+++ Notepad/Models/Form.cs
@@ -1,15 +1,21 @@
-﻿namespace Notepad.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notepad.Models
 {
     public class Form
     {
         private int _id { get; set; }
 
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public string  Email { get; set; }
 
+        [Required]
         public DateOnly DateBirth { get; set; }
 
         public Form (string firstName, string lastName, string email, DateOnly dateBirth)
---
+++ Notepad/Services/IFormService.cs
@@ -0,0 +1,20 @@
+﻿using Notepad.Models;
+
+namespace Notepad.Services
+{
+    public interface IFormService
+    {
+        Task<IEnumerable<Form>> GetAll();
+
+        Task<Form> Get(int id);
+
+        Task Add(string firstName, string lastName, string email, DateOnly birthDate);
+
+        Task Delete(int id);
+
+        Task Update(Form form);
+
+
+
+    }
+}
